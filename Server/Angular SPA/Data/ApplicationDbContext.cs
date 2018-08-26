5b76180dbf8da3024a24b0ca4524524f3e4519a2
Joar K. Mauseth <jkm99412@gmail.com>
2018-08-26 15:15:41 +0200

    Added concrete context and a seed class

---
+++ Server/Angular SPA/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using AngularSPA.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AngularSPA.Data
+{
+    public class ApplicationDbContext : DbContext, IDbContext
+    {
+        public virtual DbSet<Claim> Claim { get; set; }
+        public virtual DbSet<ClaimType> ClaimType { get; set; }
+        public virtual DbSet<Password> Password { get; set; }
+        public virtual DbSet<Person> Person { get; set; }
+        public virtual DbSet<Role> Role { get; set; }
+        public virtual DbSet<RoleClaim> RoleClaim { get; set; }
+        public virtual DbSet<User> User { get; set; }
+        public virtual DbSet<UserLogin> UserLogin { get; set; }
+        public virtual DbSet<UserToken> UserToken { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+        
+    }
+}
---
+++ Server/Angular SPA/Data/DbInitializer.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AngularSPA.Models;
+
+namespace AngularSPA.Data
+{
+    public class DbInitializer
+    {
+        private readonly IDbContext _dbContext;
+
+        public DbInitializer(IDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<IDbContext> SeedContext()
+        {
+            return await SeedRoles();
+        }
+
+        public async Task<IDbContext> SeedRoles()
+        {
+            var roles = new List<Role>
+            {
+                new Role()
+                {
+                    Name = "Super Admin"
+                },
+                new Role()
+                {
+                    Name = "Admin"
+                },
+                new Role()
+                {
+                    Name = "Regular User"
+                }
+            };
+            foreach (var role in roles)
+            {
+                role.NormalizedName = role.Name.ToUpperInvariant();
+                role.Description = $"{role.Name} role";
+            }
+
+            await _dbContext.Role.AddRangeAsync(roles);
+            _dbContext.SaveChanges();
+            return _dbContext;
+        }
+    }
+}
