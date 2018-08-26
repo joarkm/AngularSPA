ecc1ac395e39031bcb66f95ae535ccaf822255bf
Joar K. Mauseth <jkm99412@gmail.com>
2018-08-26 21:51:52 +0200

    Basic controller logic to verify login credentials

---
+++ Server/Angular SPA/Controllers/AuthController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AngularSPA.Data;
+using AngularSPA.Helpers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AngularSPA.Controllers
+{
+    [Route("api/[controller]")]
+    public class AuthController : Controller
+    {
+        private readonly IPasswordHasher _passwordHasher;
+        private readonly IDbContext _dbContext;
+
+        public AuthController(
+            IPasswordHasher passwordHasher,
+            IDbContext dbContext
+        )
+        {
+            _passwordHasher = passwordHasher;
+            _dbContext = dbContext;
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] Credentials credentials)
+        {
+            var hash = _dbContext.User
+                .Where(usr => usr.UserName.Equals(credentials.UserName))
+                .Include(usr => usr.Password)
+                .Select(usr => usr.Password.Hash)
+                .FirstOrDefault();
+            if (_passwordHasher.VerifyPassword(credentials.Password, hash))
+                return Ok();
+            return BadRequest();
+        }
+    }
+}
