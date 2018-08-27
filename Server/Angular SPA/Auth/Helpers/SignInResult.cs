50c36e27bac45f696547161d55190e5883ef4366
Joar K. Mauseth <jkm99412@gmail.com>
2018-08-27 23:36:13 +0200

    Added basic SignInManager to use in controllers

---
+++ Server/Angular SPA/Auth/Helpers/SignInResult.cs
@@ -0,0 +1,14 @@
+﻿namespace AngularSPA.Auth.Helpers
+{
+    public class SignInResult
+    {
+        public bool Succeeded { get; private set; }
+        public static SignInResult Success { get; } = new SignInResult { Succeeded = true };
+        public static SignInResult Failed()
+        {
+            return new SignInResult { Succeeded = false };
+        }
+
+        public override string ToString() => Succeeded ? "Succeeded" : "Failed";
+    }
+}
---
+++ Server/Angular SPA/Auth/SignInManager.cs
@@ -0,0 +1,32 @@
+﻿using AngularSPA.Auth.Helpers;
+using AngularSPA.Helpers;
+using AngularSPA.Models;
+using System;
+
+namespace AngularSPA.Auth
+{
+    public interface ISignInManager
+    {
+        SignInResult TrySignInWithPassword(User user, string password);
+    }
+
+    public class SignInManager : ISignInManager
+    {
+        private readonly IPasswordHasher _passwordHasher;
+
+        public SignInManager(
+            IPasswordHasher passwordHasher
+        )
+        {
+            _passwordHasher = passwordHasher;
+        }
+
+        public SignInResult TrySignInWithPassword(User user, string password)
+        {
+            if(user.Password == null)
+                throw new ArgumentNullException(nameof(user));
+            var hash = user.Password.Hash;
+            return !_passwordHasher.VerifyPassword(password, hash) ? SignInResult.Failed() : SignInResult.Success;
+        }
+    }
+}
