ed562a65c4f8775b399378e67d0032bd261c0089
Joar K. Mauseth <jkm99412@gmail.com>
2018-08-27 22:58:59 +0200

    Added Claims poco-class for seeding

--- Server/Angular SPA/Models/SeedModels/UserSeedModel.cs
+++ Server/Angular SPA/Models/SeedModels/UserSeedModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace AngularSPA.Models.SeedModels
 {
@@ -11,11 +12,21 @@ namespace AngularSPA.Models.SeedModels
         public string Password { get; set; }
         public string Role { get; set; }
         public PersonSeedModel Person { get; set; }
+        public ClaimSeedModel[] Claims { get; set; }
     }
 
     public class PersonSeedModel
     {
         public string GivenName { get; set; }
         public string SurName { get; set; }
+    }
+
+    public class ClaimSeedModel
+    {
+        [JsonProperty("Type")]
+        public string ClaimType { get; set; }
+
+        [JsonProperty("Value")]
+        public string ClaimValue { get; set; }
     }
 }
