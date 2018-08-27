61dffb7a5d7cd0f65e077e40bf6733998541a090
Joar K. Mauseth <jkm99412@gmail.com>
2018-08-27 16:22:03 +0200

    Made generic service to support standardized error handling and provide a base url

---
+++ Client/src/app/shared/services/base.service.ts
@@ -0,0 +1,55 @@
+// Based on https://github.com/mmacneil/AngularASPNETCore2WebApiAuth/blob/master/src/src/app/shared/services/base.service.ts
+
+import { Observable } from 'rxjs/Observable';
+import { HttpErrorResponse } from '@angular/common/http';
+import { ConfigService } from '../utils/config.service';
+
+export abstract class BaseService extends ConfigService {
+
+  protected baseUri: string;
+
+  constructor() {
+    super();
+    this.baseUri = this._apiURI;
+  }
+  
+  protected handleModelOrAppError(error: any) {
+    const applicationError = error.headers.get('Application-Error');
+
+    // either applicationError in header or model error in body
+    if (applicationError) {
+      return Observable.throw(applicationError);
+    }
+
+    let modelStateErrors = '';
+    const serverError = error.json();
+
+    if (!serverError.type) {
+      for (const key in serverError) {
+        if (serverError[key]) { modelStateErrors += serverError[key] + '\n'; }
+      }
+    }
+
+    modelStateErrors = modelStateErrors = '' ? null : modelStateErrors;
+    return Observable.throw(modelStateErrors || 'Server error');
+  }
+
+  protected handleError(error: HttpErrorResponse | any) {
+    let errMsg: any;
+    console.log('an error occurred!');
+
+    if (error instanceof HttpErrorResponse) {
+      const ERROR = error as HttpErrorResponse;
+      if (ERROR.status >= 500) {
+        errMsg = `${ERROR.status} ${ERROR.statusText}`;
+      } else if (ERROR.status >= 400) {
+        errMsg = 'Action failed';
+      } else if (ERROR.error.error instanceof SyntaxError) {
+        errMsg = 'Application error';
+      } else {
+        errMsg = error.message ? error.message : error.toString();
+      }
+      return Observable.throw(errMsg);
+    }
+  }
+}
